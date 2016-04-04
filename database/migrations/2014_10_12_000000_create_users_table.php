1ebc19ca3c800ce7ad126dfbc4c4d605af72e181
Francesc Fores <francescfores@iesebre.com>
2016-04-04 16:25:46 +0200

    Update usermigration and factory, added api_key

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('api_token', 128)->unique();
+
         });
     }
-
     /**
      * Reverse the migrations.
      *
